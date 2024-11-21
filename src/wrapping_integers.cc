0d451ce63c09396e8c1a02a55e4917b4f1c64edf
garlic9912 <2246257278@qq.com>
2024-11-21 07:39:39 +0000

    实现了64位索引和32位序列号的转换

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -1,19 +1,45 @@
 #include "wrapping_integers.hh"
+#include <algorithm>
+#include <iostream>
 
 using namespace std;
 
+/* Convert absolute seqno → seqno */
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
-  // Your code here.
-  (void)n;
-  (void)zero_point;
-  return Wrap32 { 0 };
+  return Wrap32(static_cast<uint32_t>(n + zero_point.raw_value_));  
 }
 
+
+/* Convert seqno → absolute seqno */
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  // Your code here.
-  (void)zero_point;
-  (void)checkpoint;
-  return {};
+  uint32_t seqno = this->raw_value_;
+  // 获得和ISN的差值
+  long long diff = seqno - zero_point.raw_value_;
+  // 获得基础的absolute seqno
+  uint64_t basic = 0;
+  if (diff < 0) basic = static_cast<uint64_t>((1UL << 32) + diff);
+  else basic = static_cast<uint64_t>(diff);
+
+  // 找到最接近checkpoint的absolute seqno
+  basic += (1UL << 32) * (checkpoint / (1UL << 32));
+  diff = abs((long long)checkpoint - (long long)basic);
+  uint64_t ret = basic;
+  // 前进一个单位
+  basic += (1UL << 32);
+  if (abs((long long)checkpoint - (long long)basic) < diff) {
+    diff = abs((long long)checkpoint - (long long)basic);
+    ret = basic;
+  }
+  basic -= (1UL << 32);
+  // 后退一个单位
+  if (basic > UINT32_MAX) { 
+    basic -= (1UL << 32);
+    if (abs((long long)checkpoint - (long long)basic) < diff) {
+      diff = abs((long long)checkpoint - (long long)basic);
+      ret = basic;
+    }    
+  }
+  return ret;
 }
--- src/wrapping_integers.hh
+++ src/wrapping_integers.hh
@@ -25,6 +25,7 @@ public:
    */
   uint64_t unwrap( Wrap32 zero_point, uint64_t checkpoint ) const;
 
+  // 操作符重载
   Wrap32 operator+( uint32_t n ) const { return Wrap32 { raw_value_ + n }; }
   bool operator==( const Wrap32& other ) const { return raw_value_ == other.raw_value_; }
 
